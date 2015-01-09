4d2041091d86a7ef3c59b9282b1b2e4fa5d3b812
rachel hazes <rahazes@rachel-hazess-MacBook-Pro-3.local>
2015-01-09 15:05:31 -0500

    first

--- rpg.rb
+++ rpg.rb
@@ -4,9 +4,9 @@ baddie_health = 10
 puts "Hey, warrior. What do they call you?"
 name = gets.chomp
 puts "So glad you're here, #{name}. Our village has been plagued by a series of terrible evils, and we desperately need you to battle them. Will you fight this snake? Y/N?"
-answer = gets.chomp
+answer = gets.chomp.upcase
 if answer == "N"
-  health_count-=2
+  health_count -= 2
   puts "You ran away. Your health count has dropped from 10 to #{health_count}."
 elsif answer == "Y"
   puts "BATTLE ROUND!"
@@ -14,6 +14,6 @@ elsif answer == "Y"
   damage_baddie = rand(1..100)
   if damage_baddie<=20
     puts "You missed!"
-    
+
 
 end
