227f9839ba481a78027ea9d5a97a12f1c8dae424
Lauren Podolnick <lauren@Adam-Podolnicks-MacBook-Pro.local>
2015-01-09 15:02:21 -0500

    second commit

--- rpg.rb
+++ rpg.rb
@@ -1,19 +1,22 @@
 health_count = 10
 baddie_health = 10
 
-puts "Hey, warrior. What do they call you?"
+puts "Hey, Beastmaster. What do they call you?"
 name = gets.chomp
-puts "So glad you're here, #{name}. Our village has been plagued by a series of terrible evils, and we desperately need you to battle them. Will you fight this snake? Y/N?"
-answer = gets.chomp
+puts "So glad you're here, #{name}. Our village has been plagued by a series of terrible beasts, and we desperately need you to gain mastery over the hordes. Will you fight this snake? Y/N?"
+answer = gets.chomp.upcase
 if answer == "N"
   health_count-=2
   puts "You ran away. Your health count has dropped from 10 to #{health_count}."
 elsif answer == "Y"
   puts "BATTLE ROUND!"
   puts "You make the first move."
-  damage_baddie = rand(1..100)
-  if damage_baddie<=20
-    puts "You missed!"
-    
-
+  damage_chance = rand(1..100) #this is the 20% chance the hero hits
+  damage_baddie = rand(2..4)
+    if damage_chance<=20
+      puts "You missed!"
+    elsif damage_chance>20
+      baddie_health-=damage_baddie
+      puts "You got a hit! The snake's health has dropped from 10 to #{baddie_health}."
+    end
 end
